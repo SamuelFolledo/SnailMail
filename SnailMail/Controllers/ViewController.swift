d77e5c6496053d3dd2111d676f57dda54836fd6f
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-06 13:53:37 -0800

    [add] codes from my delivery-organizer app available in https://github.com/SamuelFolledo/Delivery-Organizer

--- SnailMail/Controllers/ViewController.swift
+++ SnailMail/Controllers/ViewController.swift
@@ -7,14 +7,119 @@
 //
 
 import UIKit
+import MobileCoreServices
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var shippingImageView: UIImageView!
+    @IBOutlet weak var shippingTextView: UITextView!
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    var frameSublayer = CALayer()
+    var scannedText: String = "Detected text can be edited here." {
+        didSet {
+            shippingTextView.text = scannedText
+        }
+    }
+    
+    let processor = ScaledElementProcessor()
+    
+  
+//MARK: LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Notifications to slide the keyboard up
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        shippingImageView.layer.addSublayer(frameSublayer)
+        
+        displayDetectedText(in: shippingImageView)
     }
-
-
+    
+//MARK: Private methods
+    private func displayDetectedText(in imageView: UIImageView, completion: (() -> Void)? = nil) { //method that takes in the UIImageView and a callback so that you know when it's done
+        removeFrames() //remove the frames before processing a new image
+        processor.process(in: imageView) { text, elements in
+            elements.forEach() { element in
+                self.frameSublayer.addSublayer(element.shapeLayer) //this controller has a frameSublayer property that is attached to the imageView. Here, you add each element’s shape layer to the sublayer, so that iOS will automatically draw the shape on the image
+            }
+            self.scannedText = text
+        // 3
+            completion?()
+        }
+    }
+    
+    private func removeFrames() { //method that will remove sublayers, the boxes on each words
+        guard let sublayers = frameSublayer.sublayers else { return }
+        for sublayer in sublayers {
+            sublayer.removeFromSuperlayer()
+        }
+    }
+    
+// MARK: Touch handling to dismiss keyboard
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let evt = event, let tchs = evt.touches(for: view), tchs.count > 0 {
+            shippingTextView.resignFirstResponder()
+        }
+    }
+  
+// MARK: Actions
+    @IBAction func cameraButtonTapped(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            presentImagePickerController(withSourceType: .camera)
+        } else {
+            let alert = UIAlertController(title: "Camera Not Available", message: "A camera is not available. Please try picking an image from the image library instead.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func libraryButtonTapped(_ sender: Any) {
+        presentImagePickerController(withSourceType: .photoLibrary)
+    }
+    
+  
+    @IBAction func shareButtonTapped(_ sender: Any) { //creates a UIActivityVC that contains the scanned text and image then present it and let the user do the rest
+        let vc = UIActivityViewController(activityItems: [scannedText, shippingImageView.image!], applicationActivities: [])
+        present(vc, animated: true, completion: nil)
+    }
+  
+// MARK: Keyboard slide up
+    @objc func keyboardWillShow(notification: NSNotification) { //makes the view go up by keyboard's height
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if view.frame.origin.y == 0 {
+                view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+  
+    @objc func keyboardWillHide(notification: NSNotification) { //put the view back to 0
+        //if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+        if view.frame.origin.y != 0 {
+            //view.frame.origin.y += keyboardSize.height
+            view.frame.origin.y = 0
+        }
+        //}
+    }
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPopoverPresentationControllerDelegate {
+// MARK: UIImagePickerController
+    private func presentImagePickerController(withSourceType sourceType: UIImagePickerController.SourceType) {
+        let controller = UIImagePickerController()
+        controller.delegate = self
+        controller.sourceType = sourceType
+        controller.mediaTypes = [String(kUTTypeImage), String(kUTTypeMovie)]
+        present(controller, animated: true, completion: nil)
+    }
+  
+// MARK: UIImagePickerController Delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            let fixedImage = pickedImage.fixOrientation() //newly selected image is rotated back to the up position
+            shippingImageView.image = fixedImage
+            displayDetectedText(in: shippingImageView) //run our converter after getting an image
+        }
+        dismiss(animated: true, completion: nil)
+    }
+}
