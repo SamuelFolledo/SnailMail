5a40486de0d42c0500b387d85d89d7323af5286c
Genji-MS <Genji.Tapia@students.makeschool.com>
2020-01-07 13:13:58 -0800

    init python script

---
+++ SnailMail/Python/app.py
@@ -0,0 +1,11 @@
+from metaphone import doublemetaphone
+
+def insertName(name:str):
+    #lowercase
+    #remove honorifics
+    #expand shortwords using twitter code?
+    #sort alphabetically (2 or 3 words)
+    #double metaphone conversion
+    #check various metaphone tuple matches for name in database, return uID or error
+    #if uID found, send slack message.
+    pass
