da5d84083799d424ba4f83914b0b15c3d0e31093
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-07 19:45:06 -0800

    [add] added dateFormatter method and imageToData

--- SnailMail/Helper/Service.swift
+++ SnailMail/Helper/Service.swift
@@ -26,4 +26,17 @@ class Service {
         }
         on.present(alertVC, animated: true, completion: nil)
     }
+    
+    static func dateFormatter() -> DateFormatter { // DateFormatter = A formatter that converts between dates and their textual representations.
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat //dateFormat = "yyyyMMddHHmmss"
+        return dateFormatter
+    }
+
+    static func imageFromData(pictureData: String, withBlock: (_ image: UIImage?) -> Void ) { //string to image method for imageURL
+        var image: UIImage? //container for our image
+        let decodedData = NSData(base64Encoded: pictureData, options: NSData.Base64DecodingOptions(rawValue: 0)) //this will decode our string to an NSData
+        image = UIImage(data: decodedData! as Data) //assign our image to our decodedData
+        withBlock(image)
+    }
 }
