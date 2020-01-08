e685a4a43f4b304f0d989dd8524d149739e10460
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-07 19:45:29 -0800

    [add] some extensions for getting a round image, properties, etc

--- SnailMail/Helper/UIImage+extensions.swift
+++ SnailMail/Helper/UIImage+extensions.swift
@@ -9,6 +9,45 @@
 import UIKit
 
 extension UIImage {
+    
+    var isPortrait: Bool { return size.height > size.width } //RE ep.106 5mins check if portrait or landscape, and returning the height and width differences
+    var isLandscape: Bool { return size.width > size.height } //RE ep.106 5mins
+    var breadth: CGFloat { return min(size.width, size.height) } //RE ep.106 5mins returns the screen's minimum width or height
+    var breadthSize: CGSize { return CGSize(width: breadth, height: breadth) } //RE ep.106 5mins set our minimum width or height
+    var breadthRect: CGRect { return CGRect(origin: .zero, size: breadthSize) } //RE ep.106 5mins sete the rect here
+    
+    var circleMasked: UIImage? { //RE ep.106 5mins now that we have a square image, we checked if the view is portrait or landscape. Now we make the image round
+        UIGraphicsBeginImageContextWithOptions(breadthSize, false, scale) //RE ep.106 5mins
+        defer { UIGraphicsEndImageContext() } //RE ep.106 5mins if we dont have any image, dont start it
+        guard let cgImage = cgImage?.cropping(to: CGRect(origin: CGPoint(x: isLandscape ? floor( (size.width - size.height) / 2 ) : 0,
+                                                                         y: isPortrait ? floor( (size.height - size.width) / 2 ) : 0),
+                                                                        size: breadthSize)) else { return nil } //RE ep.106 5mins convert our UIImage and crop it
+        UIBezierPath(ovalIn: breadthRect).addClip() //RE ep.106 5mins create a bezier path
+        UIImage(cgImage: cgImage).draw(in: breadthRect) //RE ep.106 5mins create a UIImage from a CgImage
+        return UIGraphicsGetImageFromCurrentImageContext() //RE ep.106 5mins returns our circle image
+    }
+    
+    func scaleImageToSize(newSize: CGSize) -> UIImage { //RE ep.106 5mins
+        var scaledImageRect = CGRect.zero //RE ep.106 5mins create a
+        
+        let aspectWidth = newSize.width / size.width //RE ep.106 5mins
+        let aspectHeight = newSize.height / size.height //RE ep.106 5mins
+        
+        let aspectRatio = max(aspectWidth, aspectHeight) //RE ep.106 5mins
+        
+        scaledImageRect.size.width = size.width * aspectRatio //RE ep.106 5mins
+        scaledImageRect.size.height = size.height * aspectRatio //RE ep.106 5mins
+        scaledImageRect.origin.x = (newSize.width - scaledImageRect.size.width) / 2.0; //RE ep.106 5mins
+        scaledImageRect.origin.y = (newSize.height - scaledImageRect.size.height) / 2.0; //RE ep.106 5mins
+        
+        UIGraphicsBeginImageContext(newSize) //RE ep.106 5mins
+        draw(in: scaledImageRect) //RE ep.106 5mins
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext() //RE ep.106 5mins
+        UIGraphicsEndImageContext() //RE ep.106 5mins
+        
+        return scaledImage! //RE ep.106 5mins return our scaled image back
+    }
+    
   // Thx to: https://stackoverflow.com/questions/8915630/ios-uiimageview-how-to-handle-uiimage-image-orientation
   func fixOrientation() -> UIImage? {
     guard let cgImage = self.cgImage else {
