cc6dbf52f67c1ca2b3842deee870b259638cdaee
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-07 19:07:56 -0800

    [add] a model for Mail

---
+++ SnailMail/Model/Mail.swift
@@ -0,0 +1,13 @@
+//
+//  Mail.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/7/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import Foundation
+
+class Mail {
+    
+}
