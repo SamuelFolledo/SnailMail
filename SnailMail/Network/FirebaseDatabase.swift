e720e5932a0f5508229f54905c909b605f6acca3
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-07 17:34:06 -0800

    [add] created a file for databse

--- SnailMail/Network/FirebaseDatabase.swift
+++ SnailMail/Network/FirebaseDatabase.swift
@@ -1,9 +1,11 @@
 //
-//  NetworkDatabase.swift
+//  FirebaseDatabase.swift
 //  SnailMail
 //
 //  Created by Macbook Pro 15 on 1/7/20.
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
+import FirebaseDatabase
 import Foundation
+
