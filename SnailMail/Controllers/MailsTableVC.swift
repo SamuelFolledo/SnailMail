62c12518aea9f365b327a20ff308a0904c115e0e
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-08 12:39:35 -0800

    [add] added 3 new view controllers

---
+++ SnailMail/Controllers/MailsTableVC.swift
@@ -0,0 +1,35 @@
+//
+//  MailsTableVC.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/8/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MailsTableVC: UITableViewController {
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .black
+    }
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
+
+
---
+++ SnailMail/Controllers/SettingsVC.swift
@@ -0,0 +1,35 @@
+//
+//  SettingsVC.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/8/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+//MARK: Properties
+    
+//MARK: IBOutlets
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .black
+    }
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
+
+
