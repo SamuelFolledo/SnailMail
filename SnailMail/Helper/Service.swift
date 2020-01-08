ebad5cf1dcbcbd00c0ef30f09a7ce26d746614c4
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-07 18:05:10 -0800

    [add] present alert static method

--- SnailMail/Helper/Service.swift
+++ SnailMail/Helper/Service.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2020 SamuelFolledo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Service {    
+//presentAlert
+    static func presentAlert(on: UIViewController, title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            alertVC.dismiss(animated: true, completion: nil)
+        }
+        alertVC.addAction(okAction)
+        on.present(alertVC, animated: true, completion: nil)
+    }
+    
+    static func alertWithActions(on: UIViewController, actions: [UIAlertAction], title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        for action in actions {
+            alertVC.addAction(action)
+        }
+        on.present(alertVC, animated: true, completion: nil)
+    }
+}
