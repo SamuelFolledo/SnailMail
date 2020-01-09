b542a493251bb505bba4df810a908c959833e002
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-08 20:16:53 -0800

    [add] underlined textfield class

---
+++ SnailMail/View/UnderlinedTextField.swift
@@ -0,0 +1,37 @@
+//
+//  UnderlinedTextField.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/8/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class UnderlinedTextField: UITextField {
+    private let defaultUnderlineColor = UIColor.black
+    private let bottomLine = UIView()
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        borderStyle = .none
+        contentVerticalAlignment = .center //added
+        clearButtonMode = .unlessEditing //added
+        font = UIFont.boldSystemFont(ofSize: 18) //added
+        bottomLine.translatesAutoresizingMaskIntoConstraints = false
+        bottomLine.backgroundColor = defaultUnderlineColor
+        self.addSubview(bottomLine)
+        bottomLine.topAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true //updated to make the line closer to the text
+        bottomLine.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        bottomLine.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        bottomLine.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    }
+
+    public func setUnderlineColor(color: UIColor = .red) {
+        bottomLine.backgroundColor = color
+    }
+
+    public func setDefaultUnderlineColor() {
+        bottomLine.backgroundColor = defaultUnderlineColor
+    }
+}
