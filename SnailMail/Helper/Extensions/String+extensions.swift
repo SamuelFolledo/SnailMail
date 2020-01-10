38bddf137e1b5fd13d2741bf069e3d8034087c62
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-10 00:01:25 -0800

    [add] added a String extension that turns a multi line string into an array of string

--- SnailMail/Helper/Extensions/String+extensions.swift
+++ SnailMail/Helper/Extensions/String+extensions.swift
@@ -9,6 +9,10 @@
 import Foundation
 
 extension String {
+    var lines: [String] { //turns a multi-line string into an array of each line https://www.hackingwithswift.com/example-code/strings/how-to-get-the-lines-in-a-string-as-an-array
+        return self.components(separatedBy: "\n")
+    }
+    
     func trimmedString() -> String { //method that removes string's left and right white spaces and new lines
         let newWord: String = self.trimmingCharacters(in: .whitespacesAndNewlines)
         return newWord
