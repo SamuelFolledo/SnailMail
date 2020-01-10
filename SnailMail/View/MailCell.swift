a47c5a3ae112f4e39572c64cbc98cc0ba93b60f4
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-10 12:39:57 -0800

    [add] first commit mail cell

---
+++ SnailMail/View/MailCell.swift
@@ -0,0 +1,24 @@
+//
+//  MailCell.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/10/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class MailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
