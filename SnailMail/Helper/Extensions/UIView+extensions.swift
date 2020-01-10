436a83dace737927d6d3ce45266cba35d7a2b82b
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-10 11:46:00 -0800

    [add] a uiview animation extension

---
+++ SnailMail/Helper/Extensions/UIView+extensions.swift
@@ -0,0 +1,25 @@
+//
+//  UIView+extensions.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/10/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func enlargeThenShrinkAnimation() {
+        self.isHidden = false
+        UIView.animate(withDuration: 1.5, animations: {
+            self.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }) { (finished) in
+            UIView.animate(withDuration: 0.5, animations: {
+//                self.transform = CGAffineTransform.identity
+                self.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            }) { (finished) in
+                self.isHidden = true
+            }
+        }
+    }
+}
