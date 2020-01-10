66836b6e37d0b295556caa8fc7e12369f744ceec
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-09 19:41:45 -0800

    [add] imageDetailVC which displays image scanned

---
+++ SnailMail/View/ImageDetailVC.swift
@@ -0,0 +1,36 @@
+//
+//  ImageDetailVC.swift
+//  SnailMail
+//
+//  Created by Macbook Pro 15 on 1/9/20.
+//  Copyright © 2020 SamuelFolledo. All rights reserved.
+//
+
+import UIKit
+
+class ImageDetailVC: UIViewController {
+//MARK: Properties
+    var image: UIImage!
+//MARK: IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+//MARK: App Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+//MARK: Private Methods
+    fileprivate func setupViews() {
+        view.backgroundColor = .black
+    }
+    
+//MARK: IBActions
+    
+//MARK: Helpers
+    
+}
+
+//MARK: Extensions
+
+
