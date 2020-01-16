2c69237b8c4e2165e899bd5a07296c529c2a2b38
Samuel Folledo <samuelfolledo@gmail.com>
2020-01-15 20:18:25 -0800

    [add] remove mimeType and mimeType.hasPrefix which was not downloading images

--- SnailMail/Helper/Extensions/ImageView+extensions.swift
+++ SnailMail/Helper/Extensions/ImageView+extensions.swift
@@ -15,7 +15,7 @@ extension UIImageView {
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             guard
                 let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                //let mimeType = response?.mimeType, mimeType.hasPrefix("image"), //error here
                 let data = data, error == nil,
                 let image = UIImage(data: data)
                 else { return }
